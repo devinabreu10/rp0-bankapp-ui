3d55f6dc2291044858cdf19093d5045a3c83fe95
Devin Abreu <devinabreu10@gmail.com>
2024-07-07 16:47:06 -0500

    added unit tests for auth guard and token interceptor

--- src/app/core/auth/auth.guard.spec.ts
+++ src/app/core/auth/auth.guard.spec.ts
@@ -1,17 +1,78 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRoute,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 import { authGuard } from './auth.guard';
+import { AuthService } from './services/auth.service';
+import { Observable, of } from 'rxjs';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let authServiceMock: any;
+  let routerMock: jasmine.SpyObj<Router>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+
+  beforeEach(async () => {
+    authServiceMock = {
+      isAuthenticated: of(false) // Default value
+    };
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {},
+          },
+        },
+      ],
+    });
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow user to access route if user is not authenticated', () => {
+    authServiceMock.isAuthenticated = of(false);
+    const activatedRouteMock = TestBed.inject(ActivatedRoute);
+
+    const result = TestBed.runInInjectionContext(
+      () =>
+        authGuard(
+          activatedRouteMock.snapshot,
+          {} as RouterStateSnapshot,
+        ) as Observable<boolean>,
+    );
+
+    result.subscribe((canActivate) => {
+      expect(canActivate).toBeTrue();
+    });
+  })
+
+  it('should redirect user to home page if user is already authenticated', () => {
+    authServiceMock.isAuthenticated = of(true);
+    const activatedRouteMock = TestBed.inject(ActivatedRoute);
+
+    const result = TestBed.runInInjectionContext(
+      () =>
+        authGuard(
+          activatedRouteMock.snapshot,
+          {} as RouterStateSnapshot,
+        ) as Observable<boolean>,
+    );
+
+    result.subscribe((canActivate) => {
+      expect(canActivate).toBeFalse();
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/home']);
+    });
   });
 });
