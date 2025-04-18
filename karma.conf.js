f5e44b5d48bcbd6683d4155d77a0cecd2c7e9a08
Devin Abreu <devinabreu10@gmail.com>
2025-04-17 21:23:15 -0500

    ddd Karma config and integrate SonarQube code analysis

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/6.4/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: 'report',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },  // <== this is needed for SonarQube!
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'], // for CI environments
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
