138b5c4e65be2bc4e65364ec82e0403a7e430902
Devin Abreu <devinabreu10@gmail.com>
2025-07-11 16:23:40 -0500

    update transaction tests to cover success and error scenarios

--- src/app/features/transaction/pages/transaction/transaction.component.spec.ts
+++ src/app/features/transaction/pages/transaction/transaction.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TransactionComponent } from './transaction.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
 import { TransactionService } from '../../services/transaction.service';
 import { TransactionType } from '../../models/transaction-type.enum';
 import { Transaction } from '../../models/transaction.model';
 import { AccountService } from '../../../account/services/account.service';
+import { of, throwError } from 'rxjs';
 
 describe('TransactionComponent', () => {
   let component: TransactionComponent;
@@ -26,6 +27,7 @@ describe('TransactionComponent', () => {
       providers: [
         provideHttpClient(),
         { provide: TransactionService, useValue: transactionServiceSpy },
+        { provide: AccountService, useValue: accountServiceSpy },
       ],
     }).compileComponents();
 
@@ -63,7 +65,7 @@ describe('TransactionComponent', () => {
     );
   });
 
-  it('should deposit, withdraw, and transfer funds when form is submitted', () => {
+  it('should deposit funds when form is submitted', () => {
     // Arrange
     const transactionDeposit: Partial<Transaction> = {
       type: TransactionType.ACCOUNT_DEPOSIT,
@@ -72,9 +74,39 @@ describe('TransactionComponent', () => {
       accountNumber: 1,
     };
     component.transactionForm.patchValue(transactionDeposit);
+    accountServiceSpy.depositFunds.and.returnValue(of(''));
     component.onSubmit();
-    expect(component.transactionForm).toBeDefined();
 
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.depositFunds).toHaveBeenCalled();
+  });
+
+  it('should throw error when trying to deposit funds', () => {
+    // Arrange
+    spyOn(console, 'error');
+    const transactionDeposit: Partial<Transaction> = {
+      type: TransactionType.ACCOUNT_DEPOSIT,
+      amount: 100.0,
+      notes: 'Test notes',
+      accountNumber: 1,
+    };
+
+    component.transactionForm.patchValue(transactionDeposit);
+    accountServiceSpy.depositFunds.and.returnValue(
+      throwError(() => new Error('Error completing deposit')),
+    );
+    component.onSubmit();
+
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.depositFunds).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      'Error completing deposit',
+      new Error('Error completing deposit'),
+    );
+  });
+
+  it('should withdraw funds when form is submitted', () => {
+    // Arrange
     const transactionWithdraw: Partial<Transaction> = {
       type: TransactionType.ACCOUNT_WITHDRAW,
       amount: 100.0,
@@ -82,9 +114,65 @@ describe('TransactionComponent', () => {
       accountNumber: 1,
     };
     component.transactionForm.patchValue(transactionWithdraw);
+    accountServiceSpy.withdrawFunds.and.returnValue(of(''));
     component.onSubmit();
     expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.withdrawFunds).toHaveBeenCalled();
+  });
 
+  it('should throw error when trying to withdraw funds', () => {
+    // Arrange
+    spyOn(console, 'error');
+    const transactionWithdraw: Partial<Transaction> = {
+      type: TransactionType.ACCOUNT_WITHDRAW,
+      amount: 100.0,
+      notes: 'Test notes',
+      accountNumber: 1,
+    };
+
+    component.transactionForm.patchValue(transactionWithdraw);
+    accountServiceSpy.withdrawFunds.and.returnValue(
+      throwError(() => new Error('Error completing withdrawal')),
+    );
+    component.onSubmit();
+
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.withdrawFunds).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      'Error completing withdrawal',
+      new Error('Error completing withdrawal'),
+    );
+  });
+
+  it('should throw 409 error when trying to withdraw funds', () => {
+    // Arrange
+    spyOn(console, 'error');
+    const transactionWithdraw: Partial<Transaction> = {
+      type: TransactionType.ACCOUNT_WITHDRAW,
+      amount: 100.0,
+      notes: 'Test notes',
+      accountNumber: 1,
+    };
+
+    component.transactionForm.patchValue(transactionWithdraw);
+    accountServiceSpy.withdrawFunds.and.returnValue(
+      throwError(
+        () =>
+          new HttpErrorResponse({ status: 409, error: 'Insufficient funds for withdrawal' }),
+      ),
+    );
+    component.onSubmit();
+
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.withdrawFunds).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      'Error completing withdrawal',
+      new HttpErrorResponse({ status: 409, error: 'Insufficient funds for withdrawal' }),
+    );
+  });
+
+  it('should transfer funds when form is submitted', () => {
+    // Arrange
     const transactionTransfer: Partial<Transaction> = {
       type: TransactionType.ACCOUNT_TRANSFER,
       amount: 100.0,
@@ -92,8 +180,60 @@ describe('TransactionComponent', () => {
       accountNumber: 1,
     };
     component.transactionForm.patchValue(transactionTransfer);
+    accountServiceSpy.transferFunds.and.returnValue(of(''));
     component.onSubmit();
     expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.transferFunds).toHaveBeenCalled();
+  });
+
+  it('should throw error when trying to transfer funds', () => {
+    // Arrange
+    spyOn(console, 'error');
+    const transactionTransfer: Partial<Transaction> = {
+      type: TransactionType.ACCOUNT_TRANSFER,
+      amount: 100.0,
+      notes: 'Test notes',
+      accountNumber: 1,
+    };
+
+    component.transactionForm.patchValue(transactionTransfer);
+    accountServiceSpy.transferFunds.and.returnValue(
+      throwError(() => new Error('Error completing transfer')),
+    );
+    component.onSubmit();
+
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.transferFunds).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      'Error completing transfer',
+      new Error('Error completing transfer'),
+    );
+  });
+
+  it('should throw 409 error when trying to transfer funds', () => {
+    // Arrange
+    spyOn(console, 'error');
+    const transactionTransfer: Partial<Transaction> = {
+      type: TransactionType.ACCOUNT_TRANSFER,
+      amount: 100.0,
+      notes: 'Test notes',
+      accountNumber: 1,
+    };
+
+    component.transactionForm.patchValue(transactionTransfer);
+    accountServiceSpy.transferFunds.and.returnValue(
+      throwError(
+        () => new HttpErrorResponse({ status: 409, error: 'Insufficient funds for transfer' }),
+      ),
+    );
+    component.onSubmit();
+
+    expect(component.transactionForm).toBeDefined();
+    expect(accountServiceSpy.transferFunds).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      'Error completing transfer',
+      new HttpErrorResponse({ status: 409, error: 'Insufficient funds for transfer' }),
+    );
   });
 
   it('should set isInvalidAndTouched to false if control is invalid', () => {
